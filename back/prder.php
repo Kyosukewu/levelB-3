64ba136dfe5929d9f34bfbd54155774f0c10aa80
Kyosukewu <kyosukewu2020@gmail.com>
2021-01-15 13:31:38 +0800

    題組三完成建立假資料

--- back/prder.php
+++
@@ -1,3 +0,0 @@
-<div class="rb tab" style="width: 98%;">
-    <h2 class="ct">訂單管理</h2>
-</div>
---
+++ dumy.php
@@ -0,0 +1,35 @@
+<?php
+include_once "base.php";
+//預告片
+for($i=1;$i<=10;$i++){
+    $data['name']="預告片".$i;
+    $data['img']="03A".sprintf("%02d",$i).".jpg";
+    $data['sh']=1;
+    $data['rank']=$i+1;
+    $data['ani']=rand(1,3);
+    $Poster->save($data);
+}
+//院線片
+$level=[
+    "普遍級",
+    "輔導級",
+    "保護級",
+    "限制級"
+];
+for($i=1;$i<=10;$i++){
+    $data['name']="院線片".$i;
+    $data['trailer']="03B".sprintf("%02d",$i)."v.mp4";
+    $data['poster']="03B".sprintf("%02d",$i).".png";
+    $data['level']=$level[rand(0,3)];
+    $data['length']=100;
+    $data['year']=2021;
+    $data['month']=3;
+    $data['day']=rand(5,9);
+    $data['ondate']=$data['year']."-".$data['month']."-".$data['day'];
+    $data['publish']="院線片".$i."的發行商";
+    $data['director']="院線片".$i."的導演";
+    $data['intro']=str_repeat("院線片 $i 的劇情介紹",10);
+    $data['sh']=1;
+    $data['rank']=$i;
+    $Movie->save($data);
+}
